ed6da386330e283a127cde375f873e8be2550630
Asad Shah <me@asadmshah.com>
2016-12-18 17:42:37 +0500

    Changes to pgjdbc-ng driver.

--- database/src/main/kotlin/com/asadmshah/hnclone/server/database/DatabaseModule.kt
+++ database/src/main/kotlin/com/asadmshah/hnclone/server/database/DatabaseModule.kt
@@ -16,14 +16,15 @@ class DatabaseModule {
     @Singleton
     fun providesDataSource(configuration: Configuration): DataSource {
         val hc = HikariConfig()
-        hc.username = configuration.getString("database.user")
-        hc.password = configuration.getString("database.pass")
         hc.connectionTimeout = TimeUnit.SECONDS.toMillis(30)
         hc.maximumPoolSize = configuration.getInt("database.pool.size")
-        hc.dataSourceClassName = "org.postgresql.ds.PGSimpleDataSource"
-        hc.addDataSourceProperty("databaseName", configuration.getString("database.name"))
-        hc.addDataSourceProperty("serverName", configuration.getString("database.host"))
-        hc.addDataSourceProperty("portNumber", configuration.getString("database.port"))
+        hc.dataSourceClassName = "com.impossibl.postgres.jdbc.PGDataSource"
+        hc.addDataSourceProperty("user", configuration.getString("database.user"))
+        hc.addDataSourceProperty("password", configuration.getString("database.pass"))
+        hc.addDataSourceProperty("database", configuration.getString("database.name"))
+        hc.addDataSourceProperty("host", configuration.getString("database.host"))
+        hc.addDataSourceProperty("port", configuration.getString("database.port"))
+        hc.addDataSourceProperty("parsedSqlCacheSize", "256")
         hc.addDataSourceProperty("preparedStatementCacheQueries", "256")
         return HikariDataSource(hc)
     }
