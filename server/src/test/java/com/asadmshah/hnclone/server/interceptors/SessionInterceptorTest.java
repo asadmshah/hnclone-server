baf24a28898d08c1b1d5f5e903a879ed3b06562e
Asad Shah <me@asadmshah.com>
2016-12-22 11:45:11 +0500

    Creates Session Interceptor Test. Empty and failing for now

---
+++ server/src/test/java/com/asadmshah/hnclone/server/interceptors/SessionInterceptorTest.java
@@ -0,0 +1,29 @@
+package com.asadmshah.hnclone.server.interceptors;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.fail;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SessionInterceptorTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void shouldComplete() {
+        fail();
+    }
+
+}
