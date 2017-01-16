6fd040b82882d3b2a63463923d7955972c417a5b
Asad Shah <me@asadmshah.com>
2017-01-16 14:33:37 +0500

    Adds tests to verify that sessions removed after password change

--- database/src/test/java/com/asadmshah/hnclone/database/UsersDatabaseTest.java
+++ database/src/test/java/com/asadmshah/hnclone/database/UsersDatabaseTest.java
@@ -6,6 +6,8 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import java.time.LocalDateTime;
+
 import static com.google.common.truth.Truth.assertThat;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -18,41 +20,45 @@ public class UsersDatabaseTest extends BaseDatabaseTest {
 
     @Test
     public void test1() throws Exception {
-        UsersDatabase db = new UsersDatabaseImpl(dataSource);
+        UsersDatabase udb = new UsersDatabaseImpl(dataSource);
+        SessionsDatabase sdb = new SessionsDatabaseImpl(dataSource);
 
-        User user1 = db.create("Username 1", "Password 1", "About 1");
+        User user1 = udb.create("Username 1", "Password 1", "About 1");
         assertThat(user1).isNotNull();
-        User user2 = db.create("Username 2", "Password 2", "About 2");
+        User user2 = udb.create("Username 2", "Password 2", "About 2");
         assertThat(user2).isNotNull();
-        User user3 = db.create("Username 3", "Password 3", "About 3");
+        User user3 = udb.create("Username 3", "Password 3", "About 3");
         assertThat(user3).isNotNull();
 
-        User user1Res = db.read(1);
+        User user1Res = udb.read(1);
         assertThat(user1Res).isNotNull();
         assertThat(user1Res).isEqualTo(user1);
 
-        assertThat(db.read(-1)).isNull();
+        assertThat(udb.read(-1)).isNull();
 
-        User user2Res = db.read("Username 2");
+        User user2Res = udb.read("Username 2");
         assertThat(user2Res).isNotNull();
         assertThat(user2Res).isEqualTo(user2);
 
-        assertThat(db.read("Nonexistent")).isNull();
+        assertThat(udb.read("Nonexistent")).isNull();
 
-        User user3Res = db.read("Username 3", "Password 3");
+        User user3Res = udb.read("Username 3", "Password 3");
         assertThat(user3Res).isNotNull();
         assertThat(user3Res).isEqualTo(user3);
 
-        assertThat(db.read("Username 3", "Incorrect")).isNull();
+        assertThat(udb.read("Username 3", "Incorrect")).isNull();
 
-        assertThat(db.updateAbout(1, "Updated About 1")).isEqualTo("Updated About 1");
-        assertThat(db.read(1).getAbout()).isEqualTo("Updated About 1");
+        assertThat(udb.updateAbout(1, "Updated About 1")).isEqualTo("Updated About 1");
+        assertThat(udb.read(1).getAbout()).isEqualTo("Updated About 1");
 
-        assertThat(db.updatePassword(1, "Updated Password 1")).isTrue();
-        assertThat(db.read("Username 1", "Updated Password 1")).isNotNull();
+        assertThat(sdb.create(1, LocalDateTime.now().plusDays(1))).isNotNull();
+        assertThat(sdb.create(1, LocalDateTime.now().plusDays(1))).isNotNull();
+        assertThat(udb.updatePassword(1, "Updated Password 1")).isTrue();
+        assertThat(udb.read("Username 1", "Updated Password 1")).isNotNull();
+        assertThat(sdb.read(1).toList().blockingGet()).isEmpty();
 
-        assertThat(db.delete(1)).isTrue();
-        assertThat(db.read(1)).isNull();
-        assertThat(db.updateAbout(1, "")).isNull();
+        assertThat(udb.delete(1)).isTrue();
+        assertThat(udb.read(1)).isNull();
+        assertThat(udb.updateAbout(1, "")).isNull();
     }
 }
