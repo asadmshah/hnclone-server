e381a9f4c05cf86d23e643be0bc7f2ba5ffcc74b
Asad Shah <me@asadmshah.com>
2017-02-05 15:18:22 +0500

    Updates test cases for change in Comments interface

--- database/src/test/java/com/asadmshah/hnclone/database/CommentsDatabaseTest.java
+++ database/src/test/java/com/asadmshah/hnclone/database/CommentsDatabaseTest.java
@@ -51,20 +51,20 @@ public class CommentsDatabaseTest extends BaseDatabaseTest {
         assertThat(comment4).isNotNull();
         Comment comment5 = cdb.create(user1.getId(), post1.getId(), comment1.getId(), "Comment 5");
         assertThat(comment5).isNotNull();
-        assertThat(cdb.incrementScore(user1.getId(), comment5.getId())).isEqualTo(1);
+        assertThat(cdb.incrementScore(user1.getId(), post1.getId(), comment5.getId())).isEqualTo(1);
         Comment comment6 = cdb.create(user3.getId(), post1.getId(), comment5.getId(), "Comment 6");
         assertThat(comment6).isNotNull();
         Comment comment7 = cdb.create(user1.getId(), post1.getId(), comment6.getId(), "Comment 7");
         assertThat(comment7).isNotNull();
         Comment comment8 = cdb.create(user4.getId(), post1.getId(), comment5.getId(), "Comment 8");
         assertThat(comment8).isNotNull();
-        assertThat(cdb.incrementScore(user4.getId(), comment8.getId())).isEqualTo(1);
+        assertThat(cdb.incrementScore(user4.getId(), post1.getId(), comment8.getId())).isEqualTo(1);
         Comment comment9 = cdb.create(user1.getId(), post1.getId(), comment8.getId(), "Comment 9");
         assertThat(comment9).isNotNull();
 
         Comment comment10 = cdb.create(user1.getId(), post1.getId(), "Comment 10");
         assertThat(comment10).isNotNull();
-        assertThat(cdb.incrementScore(user1.getId(), comment10.getId())).isEqualTo(1);
+        assertThat(cdb.incrementScore(user1.getId(), post1.getId(), comment10.getId())).isEqualTo(1);
         Comment comment11 = cdb.create(user2.getId(), post1.getId(), comment10.getId(), "Comment 11");
         assertThat(comment11).isNotNull();
         Comment comment12 = cdb.create(user1.getId(), post1.getId(), comment11.getId(), "Comment 12");
@@ -73,17 +73,17 @@ public class CommentsDatabaseTest extends BaseDatabaseTest {
         assertThat(comment13).isNotNull();
         Comment comment14 = cdb.create(user3.getId(), post1.getId(), comment11.getId(), "Comment 14");
         assertThat(comment14).isNotNull();
-        assertThat(cdb.decrementScore(user3.getId(), comment14.getId())).isEqualTo(-1);
+        assertThat(cdb.decrementScore(user3.getId(), post1.getId(), comment14.getId())).isEqualTo(-1);
         Comment comment15 = cdb.create(user3.getId(), post1.getId(), comment10.getId(), "Comment 15");
         assertThat(comment15).isNotNull();
-        assertThat(cdb.decrementScore(user3.getId(), comment15.getId())).isEqualTo(-1);
+        assertThat(cdb.decrementScore(user3.getId(), post1.getId(), comment15.getId())).isEqualTo(-1);
         Comment comment16 = cdb.create(user4.getId(), post1.getId(), comment11.getId(), "Comment 16");
         assertThat(comment16).isNotNull();
         Comment comment17 = cdb.create(user1.getId(), post1.getId(), comment16.getId(), "Comment 17");
         assertThat(comment17).isNotNull();
         Comment comment18 = cdb.create(user2.getId(), post1.getId(), "Comment 18");
         assertThat(comment18).isNotNull();
-        assertThat(cdb.decrementScore(user2.getId(), comment18.getId())).isEqualTo(-1);
+        assertThat(cdb.decrementScore(user2.getId(), post1.getId(), comment18.getId())).isEqualTo(-1);
 
         List<Integer> commentsList1 = cdb.readComments(user1.getId(), post1.getId(), comment1.getId())
                 .map(Comment::getId)
